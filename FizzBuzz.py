b7dfd3921c9d84c83feffc61129a632043df771f
Andrea-tailas <146038047+Andrea-tailas@users.noreply.github.com>
2024-02-12 15:27:59 +0300

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+numbers=range(1,101)
+for i in numbers:
+    if i==i/3:
+        print('Fizz')
+    if i==i/5:
+        print('Buzz')
+    if i==i/3 and i==i/5:
+        print('FizzBuzz')
+print (i)
+      
